dbab793f141c12162baaaca4f29e21262e0fd6ec
andromeda111 <jmseymour111@gmail.com>
2018-01-10 22:37:58 -0800

    created user signup route

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db')
 const checkAuthorization = require('../services/check-authorization.middleware')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
+});
+
+router.post('/signup', function(req, res, next) {
+    let newUser = req.body;
+
+    db('users').insert(newUser, '*')
+        .then((users) => {
+            const user = users[0];
+            console.log('new user created and stored: ', user);
+            res.status(200).json({success: true, msg: 'Successful created new user: ', user});
+        }).catch(err => {
+            console.error('ERROR: ', err);
+        })
 });
 
 router.post('/verify', checkAuthorization, function(req, res, next) {
