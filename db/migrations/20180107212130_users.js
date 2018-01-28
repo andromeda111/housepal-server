93735023295b829cc7d2bdd1ad725cb0739c8247
John <jmseymour111@gmail.com>
2018-01-28 10:53:57 -0800

    set email to unique

--- db/migrations/20180107212130_users.js
+++ db/migrations/20180107212130_users.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
         table.increments()
         table.string('uid').notNullable().defaultTo('')
         table.string('name').notNullable()
-        table.string('email').notNullable()
+        table.unique('email').notNullable()
         table.string('password').notNullable()
         table.string('deviceId').defaultTo('')
         table.integer('house_id').references('id').inTable('houses').defaultTo(null).onDelete('set null')
