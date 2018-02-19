f4451a1ab821a2dbce963ef1f9fc1224786cf8e6
John <jmseymour111@gmail.com>
2018-02-18 17:34:00 -0800

    various updates

--- routes/users.js
+++ routes/users.js
@@ -9,19 +9,19 @@ router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
-// router.get('/current', checkAuthorization, function(req, res, next) {
-//     let decodedToken = req.locals.decodedToken;
-//     let uid = decodedToken.uid;
+router.get('/current', checkAuthorization, function(req, res, next) {
+    let decodedToken = req.locals.decodedToken;
+    let uid = decodedToken.uid;
 
-//     db('users').where({ uid })
-//     .then(result => {
-//         res.status(200).json(result);
-//     })
-//     .catch(err => {
-//         console.error('ERROR: ', err);
-//         //TODO: Add Error Handling
-//     })
-// });
+    db('users').where({ uid })
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.error('ERROR: ', err);
+        //TODO: Add Error Handling
+    })
+});
 
 router.post('/signin', function(req, res, next) {
     let email = req.body.email;
@@ -74,19 +74,6 @@ router.post('/signup', async function(req, res, next) {
             res.status(500).json(err)
         })
 });
-
-// router.post('/signup', function(req, res, next) {
-//     let newUser = req.body;
-
-//     db('users').insert(newUser, '*')
-//         .then((users) => {
-//             const user = users[0];
-//             console.log('new user created and stored: ', user);
-//             res.status(200).json({success: true, msg: 'Successful created new user: ', user});
-//         }).catch(err => {
-//             console.error('ERROR: ', err);
-//         })
-// });
 
 router.post('/verify', checkAuthorization, function(req, res, next) {
     let decodedToken = req.locals.decodedToken;
