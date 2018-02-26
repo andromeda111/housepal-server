e4af88f1054a3fb4e88070b1f04d177382f204d6
John <jmseymour111@gmail.com>
2018-02-25 19:52:50 -0800

    updated house seed

--- db/seeds/1_houses.js
+++ db/seeds/1_houses.js
@@ -1,23 +1,23 @@
 exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex('houses').del()
-        .then(function() {
-            // Inserts seed entries
-            return knex('houses').insert([
-                {
-                    id: 1,
-                    title: 'Tanglemansion',
-                    code: 'icecream'
-                },
-                {
-                    id: 2,
-                    title: 'Home 2',
-                    code: 'h2code'
-                }
-            ]);
-        }).then(() => {
-            return knex.raw(
-                `SELECT setval('houses_id_seq', (SELECT MAX(id) FROM houses));`
-            );
-        });
+	// Deletes ALL existing entries
+	return knex('houses')
+		.del()
+		.then(function() {
+			// Inserts seed entries
+			return knex('houses').insert([
+				{
+					id: 1,
+					houseName: 'Tanglemansion',
+					houseCode: 'icecream'
+				},
+				{
+					id: 2,
+					houseName: 'Home 2',
+					houseCode: 'h2code'
+				}
+			]);
+		})
+		.then(() => {
+			return knex.raw(`SELECT setval('houses_id_seq', (SELECT MAX(id) FROM houses));`);
+		});
 };
