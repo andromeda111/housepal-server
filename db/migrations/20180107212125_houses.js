6fbe5c02dec19bbeb51e5d7fd9a7a30702dc1e9f
John <jmseymour111@gmail.com>
2018-02-25 19:50:51 -0800

    added create and join house routes

--- db/migrations/20180107212125_houses.js
+++ db/migrations/20180107212125_houses.js
@@ -1,13 +1,15 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('houses', (table) => {
-        table.increments()
-        table.string('title').notNullable().unique()
-        table.string('code').notNullable()
-        table.timestamps(true, true)
-    })
+	return knex.schema.createTable('houses', table => {
+		table.increments();
+		table
+			.string('houseName')
+			.notNullable()
+			.unique();
+		table.string('houseCode').notNullable();
+		table.timestamps(true, true);
+	});
 };
-  
+
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('houses')
+	return knex.schema.dropTable('houses');
 };
-  
---
+++ routes/houses.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+const firebaseAdmin = require('firebase-admin');
+const checkAuthorization = require('../services/check-auth.middleware');
+
+router.post('/create', checkAuthorization, function(req, res, next) {
+	const newHouse = req.body;
+
+	db('houses')
+		.insert(newHouse)
+		.returning('*')
+		.then(result => {
+			const houseID = result[0].id;
+			const newLaundry = {
+				washer_status: false,
+				washer_start_time: { time: null },
+				washer_current_user: { id: null, name: null },
+				washer_notify: { users: null },
+				dryer_status: false,
+				dryer_start_time: { time: null },
+				dryer_current_user: { id: null, name: null },
+				dryer_notify: { users: null },
+				house_id: houseID
+			};
+			db('laundry')
+				.insert(newLaundry)
+				.returning('*')
+				.then(result => {
+					console.log('New laundry created.');
+					db('users')
+						.update({ house_id: houseID })
+						.where({ uid })
+						.returning('*')
+						.then(user => {
+							res.status(200).send({
+								success: true,
+								msg: 'House successfully created.',
+								houseID: houseID
+							});
+						});
+				});
+		})
+		.catch(err => {
+			res.status(400).send({
+				success: false,
+				msg: 'Try again; house already exists.',
+				err
+			});
+		});
+});
+
+router.post('/join', checkAuthorization, function(req, res, next) {
+	const decodedToken = req.locals.decodedToken;
+	const uid = decodedToken.uid;
+
+	const houseName = req.body.houseName;
+	const houseCode = req.body.houseCode;
+
+	db('houses')
+		.where({ title: houseName, code: houseCode })
+		.then(result => {
+			if (!result[0]) {
+				err();
+			} else {
+				const houseID = result[0].id;
+				db('users')
+					.update({ house_id: houseID })
+					.where({ uid })
+					.returning('*')
+					.then(user => {
+						res.status(200).send({
+							success: true,
+							msg: 'House successfully joined.',
+							houseID: user.house_id
+						});
+					});
+			}
+		})
+		.catch(() => {
+			res.status(400).send({ success: false, msg: 'Try again; house does not exist.' });
+		});
+});
+
+module.exports = router;
