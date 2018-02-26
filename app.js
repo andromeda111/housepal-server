8c04d1149a0c3c396b72f2d978d6a5621b4de503
John <jmseymour111@gmail.com>
2018-02-25 20:02:59 -0800

    added houses route

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ const firebaseAdmin = require('firebase-admin');
 const app = express();
 // Load Environment
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+	require('dotenv').config();
 }
 
 // Routing
 const index = require('./routes/index');
 const users = require('./routes/users');
+const houses = require('./routes/houses');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +25,7 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.enable('trust proxy')
+app.enable('trust proxy');
 app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -32,54 +33,51 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/', index);
 app.use('/users', users);
+app.use('/houses', houses);
 
 // Initialize Firebase
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert({
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID
-  }),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+	credential: firebaseAdmin.credential.cert({
+		project_id: process.env.FIREBASE_PROJECT_ID,
+		private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+		private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+		client_email: process.env.FIREBASE_CLIENT_EMAIL,
+		client_id: process.env.FIREBASE_CLIENT_ID
+	}),
+	databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 // CORS Cross Domain
 function allowCrossDomain(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, Content-Length, X-Requested-With'
-  );
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
+	if ('OPTIONS' == req.method) {
+		res.sendStatus(200);
+	} else {
+		next();
+	}
 }
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
