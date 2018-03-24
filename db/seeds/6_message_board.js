2ef1c62ea4d9799dfca34cebe00d1343d7ec7d4e
John <jmseymour111@gmail.com>
2018-03-24 12:46:03 -0700

    fixed seed

--- db/seeds/6_message_board.js
+++ db/seeds/6_message_board.js
@@ -1,31 +1,31 @@
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
     // Deletes ALL existing entries
     return knex('message_board').del()
-        .then(function() {
+        .then(function () {
             // Inserts seed entries
             return knex('message_board').insert([
                 {
                     id: 1,
-                    posterId: 2,
-                    posterName: 'Ryan',
+                    poster_id: 2,
+                    poster_name: 'Ryan',
                     content: 'Rent + utilities this month is $607.82.',
-                    postTime: {postTime: "2017-07-23T10:13:53.260Z"},
+                    post_time: { post_time: '2017-07-23T10:13:53.260Z' },
                     house_id: 1
                 },
                 {
                     id: 2,
-                    posterId: 0,
-                    posterName: 'App Notification',
-                    content: 'Lindsey added "Coffee Filters" to the communal shopping list.',
-                    postTime: {postTime: "2017-07-24T17:47:53.260Z"},
+                    poster_id: 0,
+                    poster_name: 'App Notification',
+                    content: `Lindsey added 'Coffee Filters' to the communal shopping list.`,
+                    post_time: { postTime: '2017-07-24T17:47:53.260Z' },
                     house_id: 1
                 },
                 {
                     id: 3,
-                    posterId: 3,
-                    posterName: 'Cassa',
+                    poster_id: 3,
+                    poster_name: 'Cassa',
                     content: 'Hey guys! I have an avocado I won\'t have time to eat before I leave for the weekend. It\'s up for grabs on the kitchen counter!',
-                    postTime: {postTime: "2017-07-25T14:35:53.260Z"},
+                    post_time: { postTime: '2017-07-25T14:35:53.260Z' },
                     house_id: 1
                 }
             ]);
