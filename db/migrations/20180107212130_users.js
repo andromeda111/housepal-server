cc45294eb1af4f899306e7cbe14288d67c17dc5d
andromeda111 <jmseymour111@gmail.com>
2018-06-10 21:15:01 -0700

    added unique to user uid in table

--- db/migrations/20180107212130_users.js
+++ db/migrations/20180107212130_users.js
@@ -1,7 +1,7 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('users', (table) => {
         table.increments()
-        table.string('uid').notNullable().defaultTo('')
+        table.string('uid').unique().notNullable().defaultTo('')
         table.string('name').notNullable()
         table.string('email').notNullable().unique()
         table.string('password').notNullable()
